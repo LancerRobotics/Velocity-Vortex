65834889d6d04cf89efa6910dd16a255e3ef2b54
dbchess <dina_mathlete@yahoo.com>
2016-10-17 15:47:25 -0400

    To pull

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/GoToRightBeacon.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.actualCode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Keys;
+import org.firstinspires.ftc.teamcode.LancerLinearOpMode;
+
+/**
+ * Created by AMAMbI4 on 10/8/2016.
+ */
+
+public class GoToRightBeacon extends LancerLinearOpMode{
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor br;
+    DcMotor bl;
+    public void runOpMode() throws InterruptedException{
+        goRight(.86);
+    }
+    public void goRight(double power){
+        fl.setPower(power);
+        br.setPower(power);
+        try {
+            Thread.sleep(3000);
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        fl.setPower(0);
+        br.setPower(0);
+    }
+
+}

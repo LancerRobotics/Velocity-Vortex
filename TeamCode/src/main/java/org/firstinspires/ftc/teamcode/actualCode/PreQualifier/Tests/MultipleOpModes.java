eb99698eef07a8af4a6a82046b309aff31eec4fc
andrew.keenan <andrew.keenan@livingston.org>
2016-11-13 21:05:21 -0500

    Instructions and MultipleOpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/Tests/MultipleOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/Tests/MultipleOpModes.java
@@ -1,38 +1,34 @@
 package org.firstinspires.ftc.teamcode.actualCode.PreQualifier.Tests;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import org.firstinspires.ftc.teamcode.LancerLinearOpMode;
 
 /**
  * Created by shlok.khandelwal on 11/9/2016.
  */
 
+
 //DO NOT BUILD TO PHONES UNLESS THIS IS COMMENTED OUT IN ORDER TO AVOID MEMORY PROBLEMS, AS SOKOLOV MENTIONED THAT THIS
 //CODE BEING ON THE PHONES WILL CAUSE DAMAGE WITHOUT BEING RUN.
 
 /*
 //DO NOT USE THIS UNTIL WE EITHER FIGURE OUT HOW TO RUN TWO OPMODES AT ONCE OR SOKOLOV EXPLAINS HOW TO RUN MULTIPLE OPMODES AT ONCE
-public class MultiThreading extends LancerLinearOpMode {
+public class MultipleOpModes extends LancerLinearOpMode {
 
     @Override
     public void runOpMode() {
         setup();
-
-
-    }
-}
-class RunnableDemo implements Runnable{
-    private Thread t;
-    private String threadName;
-    RunnableDemo(String name){
-        threadName = name;
-        System.out.println("Creating " + threadName);
-
-    }
-
-    @Override
-    public void run() {
-
+        OpMode2.runOpMode();
     }
 }
 
+class OpMode2 extends LinearOpMode {
+
+    public static void runOpMode() {
+
+    }
+
+}
 */

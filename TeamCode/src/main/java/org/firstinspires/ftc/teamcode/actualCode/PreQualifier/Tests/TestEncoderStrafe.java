45a56959457d91a01378f56f3bbbfba409c0847b
shlok.khandelwal <shlok.khandelwal@LIVINGSTON.ORG>
2016-11-15 16:56:16 -0500

    EncoderStrafed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/Tests/TestEncoderStrafe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/Tests/TestEncoderStrafe.java
@@ -22,14 +22,70 @@ public class TestEncoderStrafe extends LancerLinearOpMode{
         waitForStart();
         startUp();
         //moveLeft
-        moveSideToSide(20,true,.4);
+        strafe(20,true,.4);
         rest();
         sleep(2000);
         //moveRight
-        moveSideToSide(10,false,.1);
+        strafe(10,false,.1);
         rest();
         sleep(2000);
     }
+    public void strafe(double inches, boolean left, double power){
+
+            double inches_per_rev = 560.0 / (Keys.WHEEL_DIAMETER * Math.PI);
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            int newLeftTarget = fl.getCurrentPosition() + (int)(inches * inches_per_rev);
+            int newRightTarget = br.getCurrentPosition() + (int)(inches * inches_per_rev);
+            fl.setTargetPosition(newLeftTarget);
+            br.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            if(left){
+                fr.setPower(power*-1.0);
+                fl.setPower(power);
+                br.setPower(power);
+                bl.setPower(power*-1.0);
+            }else {
+                fr.setPower(power);
+                fl.setPower(power*-1.0);
+                br.setPower(power*-1.0);
+                bl.setPower(power);
+            }
+
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (fl.isBusy() && br.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        fl.getCurrentPosition(),
+                        br.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            fl.setPower(0);
+            br.setPower(0);
+            fr.setPower(0);
+            bl.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+        }
+
 
     public void moveSideToSide(double inches, boolean left, double power) {
         double inches_per_rev = 560.0 / (Keys.WHEEL_DIAMETER * Math.PI);//Test to find correct conversion factor for WheelDiam(leftnRight)

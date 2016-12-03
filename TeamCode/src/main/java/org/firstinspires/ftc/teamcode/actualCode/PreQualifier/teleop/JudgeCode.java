68249757af49d9363172f1d203ce07a98357dbec
dina.brustein <dina.brustein@livingston.org>
2016-12-02 20:55:52 -0500

    finished judges code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/teleop/JudgeCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/teleop/JudgeCode.java
@@ -16,10 +16,23 @@ public class JudgeCode extends LancerOpMode{
     public void init (){
 
     }
+
     public void runOpMode(){
         if(gamepad1.a){
+            //collect and shoot
             collector.setPower(0.86);
-            shoot();
+            shoot(0.99);
+        }
+
+        if(gamepad1.b){
+            //turn everything off
+            collector.setPower(0);
+            shoot(0);
+        }
+
+        if(gamepad1.x){
+            //lift turns on
+            lift.setPower(0.86);
         }
     }
 }

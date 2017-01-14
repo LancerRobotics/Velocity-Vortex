e58a95b5254771b4ee1c12273d4dbabf16b55fb0
dina.brustein <dina.brustein@LIVINGSTON.ORG>
2017-01-14 16:58:26 -0500

    White line tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/Tests/WhiteLineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actualCode/PreQualifier/Tests/WhiteLineTest.java
@@ -1,20 +1,37 @@
 package org.firstinspires.ftc.teamcode.actualCode.PreQualifier.Tests;
 
+import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import org.firstinspires.ftc.teamcode.Keys;
 
 import org.firstinspires.ftc.teamcode.LancerLinearOpMode;
 
 /**
  * Created by dina.brustein on 1/9/2017.
  */
-
+@Autonomous(name = "WhiteLineTest", group = "Autonomous")
 public class WhiteLineTest extends LancerLinearOpMode{
-    public void runOpMode() {
-        waitForStart();
-        setup();
+    public static AHRS dim = navx_device;
+    @Override
+    public void setup(){
+        I2cDeviceSynch colorCreader;
+        telemetryAddData("Status", "Initialized");
 
-        public boolean findFirstWhiteLine(boolean firstWhiteLine) {
+    }
+    public void runOpMode(){
+        setup();
+        waitForStart();
+        boolean firstWhiteLine = findFirstWhiteLine();
+        boolean secondWhiteLine = findSecondWhiteLine();
+    }
+
+        public boolean findFirstWhiteLine() {
             detectColor();
+            boolean firstWhiteLine = false;
             if (red == 2 && blue == 2 && green == 2) {
                 firstWhiteLine = true;
                 telemetryAddLine("White line detected");
@@ -23,13 +40,13 @@ public class WhiteLineTest extends LancerLinearOpMode{
                 firstWhiteLine = false;
                 telemetryAddLine("No white line detected");
             }
-            return(firstWhiteLine);
+            return firstWhiteLine;
     }
-        public boolean findSecondWhiteLine(boolean secondWhiteLine) {
-            secondWhiteLine = false;
+        public boolean findSecondWhiteLine() {
+            boolean secondWhiteLine = false;
             sleep(500);
             detectColor();
-            if(red == 2 && blue == 2 && green ==2){
+            if(red == 2 && blue == 2 && green == 2){
                 secondWhiteLine = true;
                 telemetryAddLine("White line detected");
             }
@@ -40,36 +57,40 @@ public class WhiteLineTest extends LancerLinearOpMode{
             return(secondWhiteLine);
     }
 
-        public boolean findCenterBase(boolean centerBase, boolean blueAlliance){
-            if(blueAlliance){
+        public boolean findCenterBase(boolean blueAlliance) {
+            boolean centerBase = false;
+            if (blueAlliance) {
                 detectColor();
-                if(blue == 2){
-                //stop robot
+                if (blue == 2) {
+                    //stop robot
+                    centerBase = true;
                     telemetryAddLine("Blue center tape detected");
                 }
-                else{
+                else {
+                    centerBase = false;
                     telemetryAddLine("Blue center tape not detected");
                 }
-        }
+            }
 
             else if(!blueAlliance){
                 detectColor();
                 if(red == 2){
                 //stop robot
+                    centerBase = true;
                     telemetryAddLine("Red center tape detected");
                 }
 
                 else{
+                    centerBase = false;
                     telemetryAddLine("Red center tape not detected");
                 }
             }
             return(centerBase);
+        }
+
     }
 
-}
 
-
-}
 
     
 
